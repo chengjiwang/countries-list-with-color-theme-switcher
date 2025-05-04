0590d13271431692f09bf58321be79d52eca74a4
Alvin Wang <chengji3728@gmail.com>
2025-05-04 12:25:37 +0800

    feat: refactor country data fetching and move to separate query file

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 const url = "https://restcountries.com/v3.1";
+export const fields =
+  "name,flags,population,region,subregion,capital,currencies,languages,tld";
 
 export const api = axios.create({ baseURL: url });
