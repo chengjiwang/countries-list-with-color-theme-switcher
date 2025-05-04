04958c238409b938392319d26b0bbbf44254edd2
Alvin Wang <chengji3728@gmail.com>
2025-05-04 15:11:36 +0800

    feat: implement detail page with country data fetching

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -1,3 +1,27 @@
+import getSearchNameOptions from "@/query/getSearchNameOptions";
+import { Box, CircularProgress } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router";
+
 export default function Detail() {
+  const { countryName } = useParams();
+  const { data, isLoading } = useQuery(getSearchNameOptions(countryName));
+  console.log("data", data);
+
+  if (isLoading) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          height: "100vh",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+
   return <div>Detail</div>;
 }
